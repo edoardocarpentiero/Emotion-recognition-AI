eae86e06cc07508bf411df91edfbf36710077103
edoardocarpentiero <edoardo93av@gmail.com>
2025-04-24 20:31:22 +0200

    Modifica impostazione del main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import model.WebcamDetection
 if __name__ == "__main__":
 
 
-    model.ModelTraining.runTraining('assets/train', 'assets/dataset_bilanciato_fused',4)
-    model.ModelEvaluation.evaluateModel('assets/test',['ambiguous', 'negative', 'neutral', 'positive'])
+    #model.ModelTraining.runTraining('assets/train', 'assets/dataset_bilanciato_fused',4)
+    #model.ModelEvaluation.evaluateModel('assets/test',['ambiguous', 'negative', 'neutral', 'positive'])
 
-    model.WebcamDetection.webcam(true, ['ambiguous', 'negative', 'neutral', 'positive'])
+   model.WebcamDetection.webcam(['ambiguous', 'negative', 'neutral', 'positive'])
